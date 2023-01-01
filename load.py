4a481ce1903794d4b41390f3503f006779925b9a
DanielPysz <64737379+DanielPysz@users.noreply.github.com>
2023-01-01 13:33:00 +0100

    Update load.py

--- load.py
+++ load.py
@@ -1,27 +1,28 @@
 import csv
+import pandas as pd
+import numpy as np
 
 def open_CO2(file_path):
-    lst = []
-    dzCO2 = {}
-    with open(file_path, "r") as co2:
-        csread = csv.reader(co2, delimiter = ",")
-        table = list(csread)
-        a = table[1:][17231][1]
-        for i in range(len(table)-2):
-            if table[1:][i][1] in lst:
-                pass
-            else:
-                lst.append(table[1:][i][1])
-        for country in lst:
-            ems = {}
-            for y in range(len(table)-2):
-                if table[1:][y][1] == lst[lst.index(country)]:
-                    ems[table[1:][y][0]] = table[1:][y][2]
-                else:
-                    pass
-            dzCO2[country] = ems
+    co2 = pd.read_csv(file_path)
+    index = np.arange(1,17234, dtype=int)
+    years = np.arange(1751,2015, dtype=int)
+    a = co2['Year'] == 2014
+    b = co2[a]
+    #print(b.sort_values(by=['Per Capita'], ascending=False))
 
-    print(dzCO2)
+def open_GDP(file_path):
+    gdp = pd.read_csv(file_path)
+    gdpnow = gdp['2021'] > 0
+    gdp2021 = gdp[gdpnow]
+    #print(gdp)
+    return gdp2021
+def open_pop(file_path):
+    pop = pd.read_csv(file_path)
+    popnow = pop['2021'] > 0
+    pop2021 = pop[popnow]
+    return pop2021
+
+
 open_CO2("C:/Users/danpy/OneDrive/Pulpit/co2-fossil-by-nation_zip/data/fossil-fuel-co2-emissions-by-nation_csv.csv")
-
-
+open_GDP("C:/Users/danpy/OneDrive/Pulpit/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562.csv")
+#open_pop("C:/Users/danpy/OneDrive/Pulpit/API_SP.POP.TOTL_DS2_en_csv_v2_4751604/API_SP.POP.TOTL_DS2_en_csv_v2_4751604.csv")
